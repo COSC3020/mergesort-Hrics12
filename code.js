a17699ad3433a2e95f9a12779abffd2b07cb4116
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-04-25 12:23:51 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,26 @@
-function mergesort(array) {
-    return array;
+function mergeSort(array) {
+  if (array.length < 2) return array;
+  let temp = new Array(array.length);
+  for (let size = 1; size < array.length; size *= 2) {
+    for (let LStart = 0; LStart < array.length; LStart += 2*size) {
+      let L = LStart, R = Math.min(L + size, array.length), end = Math.min(R + size, array.length), tempIndex = L;
+      while (L < R && R < end) {
+        if (array[L] <= array[R]) {
+          temp[tempIndex++] = array[L++];
+        } else {
+          temp[tempIndex++] = array[R++];
+        }
+      }
+      while (L < R) {
+        temp[tempIndex++] = array[L++];
+      }
+      while (R < end) {
+        temp[tempIndex++] = array[R++];
+      }
+    }
+    let swap = array;
+    array = temp;
+    temp = swap;
+  }
+  return array;
 }
