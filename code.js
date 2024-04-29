28afa4397c08df2228ffbda4706d78abbc0f9a49
Hrics12 <111898293+Hrics12@users.noreply.github.com>
2024-04-29 14:35:49 -0600

    Update code.js

--- code.js
+++ code.js
@@ -3,35 +3,39 @@ function mergesort(array) {
     let leftStart
     let arraySize = array.length
 
-    for (let currentSize = 1; currentSize < array.length; currentSize = currentSize * 2) {
-        for (let L = 0; L < array.length; L += 2 * currentSize) {
-            let R = Math.min(L + currentSize, array.length);
-            let endR = Math.min(R + currentSize, array.length);
-            let i = L;
-            let j = R;
-            let k = L;
-
-            while (i < R && j < endR) {
-                if (array[i] <= array[j]) {
-                    array2[k++] = array[i++];
-                } else {
-                    array2[k++] = array[j++];
-                }
-            }
-
-            while (i < R) {
-                array2[k++] = array[i++];
-            }
-
-            while (j < endR) {
-                array2[k++] = array[j++];
-            }
+    for (let currentSize = 1; currentSize <= arraySize -1; currentSize = currentSize * 2) {
+        for (let leftStart = 0; leftStart < arraySize; leftStart += 2 * currentSize) {
+            let mid = Math.min(leftStart + currentSize - 1, arraySize - 1);
+            let rightEnd = Math.min(leftStart + 2 * currentSize - 1, arraySize - 1);
+            merge(array, leftStart, mid, rightEnd)
         }
-
-        let temp = array;
-        array = array2;
-        array2 = temp;
     }
-
     return array;
 }
+function merge(array, left, mid, right){
+
+let x = mid + 1;
+if(array[mid] <= array[x]){
+    return;
+}
+    while (left <= mid && x <= right){
+        if(array[left] <= array[x]){
+            left++;
+        }
+        else{
+            let y = array[x];
+            let index = x;
+
+            while(index != left){
+                array[index] = array[index -1];
+                index--
+            }
+            array[left] = value;
+            left++;
+            mid++;
+            x++;
+        }
+    }
+}
+
+      
